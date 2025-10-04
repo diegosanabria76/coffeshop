3d4ebe2e31a6b3b7f0631ad40303823c3946fdc5
diegosanabria76@gmail.com <diegosanabria76@gmail.com>
2025-10-04 03:54:08 +0000

    mi primer commit desde ceros

---
+++ coffee_shop.py
@@ -0,0 +1,30 @@
+MENU = {
+     "espresso" : {
+	"ingredients":{
+		"water":100,
+		"coffee":120,
+		},
+   	 "cost":1.5,
+	},
+ 	"Latte" : { 
+        	"ingredients":{
+                "water":100,
+                "coffee":120,
+		"milk": 80,
+                },
+         "cost":2.5,
+        },
+ 	"capuccino" : { 
+        	"ingredients":{
+                "water":100,
+                "coffee":120,
+		"milk":50,
+                },
+         "cost":4.7,
+        }
+}
+
+profit = 0
+
+print(MENU)
+
